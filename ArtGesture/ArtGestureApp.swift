54c41178e5667319a3eca20f4efd6637fcf0761d
zhangjunbao <zhangjunbao@unipus.cn>
2024-05-04 21:33:22 +0800

    Initial Commit

---
+++ ArtGesture/ArtGestureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtGestureApp.swift
+//  ArtGesture
+//
+//  Created by junbaor on 2024/5/4.
+//
+
+import SwiftUI
+
+@main
+struct ArtGestureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
